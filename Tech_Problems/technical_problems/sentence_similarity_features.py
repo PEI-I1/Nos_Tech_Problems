cb6ba05790ebd4992e76c9d6b9b0be0ea2fcdc13
JoaoVieira97 <jonasv55@hotmail.com>
2019-12-04 21:08:46 +0000

    First approach to use the technical problems model. To improve.

---
+++ Tech_Problems/technical_problems/sentence_similarity_features.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import tensorflow_hub as hub
+import numpy as np
+import tensorflow_text
+import json
+import fileinput
+import re
+from .keywords import keywords
+import os
+
+
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/2")
+
+
+def loadProblems(feature_name):
+    with open(os.getcwd() + '/technical_problems/model_files/input_options.json') as json_file:
+        data = json.load(json_file)
+        feature_og = [x for x in data[feature_name]]
+        feature = [x.lower() for x in feature_og]
+    return embed(feature)["outputs"], feature_og
+
+
+result_sintoma, sintoma = loadProblems('Sintoma')
+result_tipificacao_1, tipificacao_tipo_1 = loadProblems('Tipificacao_Nivel_1')
+result_tipificacao_2, tipificacao_tipo_2 = loadProblems('Tipificacao_Nivel_2')
+result_tipificacao_3, tipificacao_tipo_3 = loadProblems('Tipificacao_Nivel_3')
+print('-------------> READY <-------------')
+
+def replaceWithKeywords(line, keyword_data):
+    keyword_versions = [line]
+    for keyword, matches in keyword_data.items():
+        keyword_versions.extend([re.sub(match, keyword, line) for match in matches if re.search(match, line)])
+
+    #print(keyword_versions)
+    return keyword_versions
+    
+
+def getProblem(features):
+    data = keywords
+    response = []
+    i = 0
+    for line in features:
+        if i == 0:
+            result_tipificacao = result_sintoma
+            tipificacao_tipo = sintoma
+        elif i == 1:
+            result_tipificacao = result_tipificacao_1
+            tipificacao_tipo = tipificacao_tipo_1
+        elif i == 2:
+            result_tipificacao = result_tipificacao_2
+            tipificacao_tipo = tipificacao_tipo_2
+        else:
+            result_tipificacao = result_tipificacao_3
+            tipificacao_tipo = tipificacao_tipo_3
+        line_versions = replaceWithKeywords(line.lower(), data)
+        result_sentences =  [embed(line_version)["outputs"] for line_version in line_versions]
+        similarity_matrices = [list(np.inner(result_sentence, result_tipificacao)[0]) for result_sentence in result_sentences]
+        max_values = [max(similarity_matrice) for similarity_matrice in similarity_matrices]
+        max_abs = max(max_values)
+        similarity_matrix = similarity_matrices[max_values.index(max_abs)]
+        sugestao = tipificacao_tipo[similarity_matrix.index(max_abs)]
+        response.append((sugestao, str(max_abs)))
+        i += 1
+    return response
+    #print(response)
+
+
+'''
+print("Introduza o seu problema técnico:")
+for line in fileinput.input():
+    features = line.split(',')
+    getProblem(features)
+'''
--- Tech_Problems/technical_problems/views.py
+++ Tech_Problems/technical_problems/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 import json
 import os
 from .model_prediction import load_model, predict_resolution, load_dict
+from .sentence_similarity_features import getProblem
 
 model = load_model(os.getcwd() + '/technical_problems/model_files/model')
 
@@ -22,7 +23,7 @@ def authenticate(request):
 def solve(request):
     #TODO, check that user is authenticated
 
-    phone_number = "933333331"
+    phone_number = "933333333"
     nif = "111111111"
 
     sintoma = request.GET.get('sintoma', '')
@@ -32,32 +33,51 @@ def solve(request):
     servico = request.GET.get('servico', '')
 
     if sintoma and tipificacao_tipo_1 and tipificacao_tipo_2 and tipificacao_tipo_3 and servico:
-
-        clients = Client.objects \
-                       .filter(username=phone_number, password=nif) \
-                       .values_list('equipamento_tipo__name', 'tarifario__name')
-
-        if clients:
-            client = clients[0]
-            equipamento = client[0]
-            tarifario = client[1]
-
-            input = [
-                equipamento,
-                servico,
-                sintoma,
-                tarifario,
-                tipificacao_tipo_1,
-                tipificacao_tipo_2,
-                tipificacao_tipo_3,
-            ]
-            
-            prediction,probability = predict_resolution(input,model) #'Desliga e volta a ligar', 0.56 
-
-            response_as_json = json.dumps({'prediction': prediction, 'probability': probability})
         
+        if servico in ['TV', 'Internet', 'Voz']:
+
+            clients = Client.objects \
+                        .filter(username=phone_number, password=nif) \
+                        .values_list('equipamento_tipo__name', 'tarifario__name')
+
+            if clients:
+                client = clients[0]
+                equipamento = client[0]
+                tarifario = client[1]
+
+                problem = getProblem([sintoma, tipificacao_tipo_1, tipificacao_tipo_2, tipificacao_tipo_3])
+                sintoma = problem[0][0]
+                tipificacao_tipo_1 = problem[1][0]
+                tipificacao_tipo_2 = problem[2][0]
+                tipificacao_tipo_3 = problem[3][0]
+                print()
+
+                input = [
+                    equipamento,
+                    servico,
+                    problem[0][0], # Sintoma
+                    tarifario,
+                    problem[1][0], # Tipificação Nivel 1
+                    problem[2][0], # Tipificação Nivel 2
+                    problem[3][0], # Tipificação Nivel 3
+                ]
+                
+                prediction,probability = predict_resolution(input,model) #'Desliga e volta a ligar', 0.56 
+
+                similarity_features = {
+                    'sintoma': {'sugestão': problem[0][0], 'certeza': problem[0][1]},
+                    'tipificacao_1': {'sugestão': problem[1][0], 'certeza': problem[1][1]},
+                    'tipificacao_2': {'sugestão': problem[2][0], 'certeza': problem[2][1]},
+                    'tipificacao_3': {'sugestão': problem[3][0], 'certeza': problem[3][1]},
+                }
+
+                response_as_json = json.dumps({'similarity': similarity_features, 'prediction': prediction, 'probability': probability})
+            
+            else:
+                response_as_json = json.dumps({'error': 'Can\'t find client'})
+
         else:
-            response_as_json = json.dumps({'error': 'Can\'t find client'})
+            response_as_json = json.dumps({'error': 'Parameter \'servico\' has to be \'TV\', \'Internet\' or \'Voz\''})
     
     else:
         response_as_json = json.dumps({'error': 'Bad parameters'})
