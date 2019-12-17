8ca94c9cde575c0a7c47b294f2d0b7b1a0082f77
MQuaresma <miguelquaresma97@gmail.com>
2019-12-17 10:09:18 +0000

    Add support for more than one contract to be associated to a single client

--- technical_problems/models.py
+++ technical_problems/models.py
@@ -3,34 +3,42 @@ from django.contrib.auth.models import User
 
 class Equipamento_Tipo(models.Model):
     name = models.CharField(max_length=256)
+    AVAILABLE_SERVICES = [
+        (1, 'TV'),
+        (2, 'Internet'),
+        (3, 'Voz')
+    ]
+    service = models.IntegerField(
+        choices=AVAILABLE_SERVICES,
+        default=1
+    )
     def get_by_natural_key(self, equipamento_tipo):
         return self.get(name=equipamento_tipo)
+
 
 class Tarifario(models.Model):
     name = models.CharField(max_length=256)
     def get_by_natural_key(self, tarifario):
         return self.get(name=tarifario)
 
-class ServiceType(models.Model):
-    AVAILABLE_SERVICES = [
-        (1,  'TV'),
-        (2, 'Internet'),
-        (3, 'Voz')
-    ]
-    name = models.IntegerField(
-        choices=AVAILABLE_SERVICES,
-        default=1
-    )
-    def get_by_natural_key(self, service):
-        return self.get(name=service)
 
-class Client(models.Model):
-    """ NOS client
-    """
-    #username = models.CharField(max_length=256)
-    #password = models.CharField(max_length=256)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=256)
-    services = models.ManyToManyField(ServiceType)
+class Contract(models.Model):
+    ''' ISP contract associated with a specific address
+    '''
+    address = models.CharField(primary_key=True, max_length=256)
+    tariffs = models.ForeignKey(
+        'Tarifario',
+        on_delete=models.DO_NOTHING
+    )
     devices = models.ManyToManyField(Equipamento_Tipo)
-    tariffs = models.ManyToManyField(Tarifario)
+
+    
+class Client(models.Model):
+    ''' NOS client
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # contracts = models.ManyToManyField(Contract) - support more than one contract
+    contract = models.ForeignKey(
+        'Contract',
+        on_delete=models.CASCADE,
+    )
