5fd0d68f8e5e00c9899ef5fe599a35e719f26de0
JoaoVieira97 <jonasv55@hotmail.com>
2020-01-15 23:55:02 +0000

    Changes on sentences to be more friendly. Other small changes (related do similarity and model retrain).

--- NTP_Bot/ids.py
+++ NTP_Bot/ids.py
@@ -112,7 +112,7 @@ class Inter_State:
             cs = cs[input_arg]
             search_space = [search_tree for search_tree in cs]
             mt, prob = msg_interpreter.extractProblemData(prob_desc, search_space, 0)
-            if prob < 0.65: #FIXME: change threshold to 0.65
+            if prob < 0.4:
                 #print(self.model_args)
                 return False
             elif prob > self.model_args[input_arg][1]:
--- NTP_Bot/settings.py
+++ NTP_Bot/settings.py
@@ -1,16 +1,16 @@
 UPROMPT = [
-    'Por favor indique o seu nº de telemóvel seguido do NIF.',
-    'Indique em qual dos seguintes serviços necessita de assistência: TV, Internet, Voz.',
-    'Descreva o problema em causa.',
-    'Não foi possível reconhecer qual o serviço a que se refere, tente novamente.',
-    'Não foi possível obter dados de um cliente com os dados que inseriu. Envie novamente o seu nº de telemóvel e o NIF separados por um espaço.',
-    'Aconteceu um erro ao efetuar a operação de autenticação. Envie novamente o seu nº de telemóvel e o NIF separados por um espaço.',
-    'Não foi possível reconhecer o problema descrito, tente novamente.',
+    'É necessário realizar uma autenticação que mostre que és cliente da NOS.\nIndica o teu nº de telemóvel e o NIF separados por um espaço (exemplo \'930000000 123456789\').',
+    'Indica em qual dos seguintes serviços necessitas de assistência: TV, Internet, Voz.',
+    'Descreve o problema em que necessitas de ajuda.',
+    'Não foi possível reconhecer qual o serviço a que te referes, tenta novamente (TV, Internet ou Voz).',
+    'Não foi possível obter dados de um cliente com os dados que inseriste. Envia novamente o teu nº de telemóvel e o NIF separados por um espaço (exemplo \'930000000 123456789\').',
+    'Aconteceu um erro ao efetuar a operação de autenticação. Envia novamente o teu nº de telemóvel e o NIF separados por um espaço (exemplo \'930000000 123456789\').',
+    'Não foi possível reconhecer o problema descrito, tenta descrever o mesmo novamente.',
     'Foi atingido o máximo de tentativas para esta etapa do processo. A sair do modo de resolução de problemas técnicos...',
-    'Não foi possível reconhecer a sua mensagem. A sugestão resolveu o seu problema? (sim/não)',
+    'Não foi possível reconhecer a tua mensagem. A sugestão resolveu o seu problema? (sim/não)',
     'Ainda bem que o seu problema ficou resolvido. A sair do modo de resolução de problemas técnicos...',
-    'Não existem mais sugestões para resolver o seu problema. A sair do modo de resolução de problemas técnicos...',
-    'Não tem esse tipo de serviço contratado. Por favor tente novamente.'
+    'Não existem mais sugestões para resolver o seu problema, recomendamos que entre em contacto com as linhas de apoio. A sair do modo de resolução de problemas técnicos...',
+    'Não tens esse tipo de serviço contratado. Por favor, tenta novamente (TV, Internet ou Voz).'
 ]
 SOLVER_ENDPOINT = 'http://{}:{}/problems/'
 SOLVER_ENDPOINT_LOGIN = SOLVER_ENDPOINT + 'login'
@@ -19,6 +19,6 @@ SOLVER_ENDPOINT_SERVICE_CHECK = SOLVER_ENDPOINT + 'client_has_service'
 SOLVER_ENDPOINT_UPDATE_LOG = SOLVER_ENDPOINT + 'update_log'
 MAX_ERROR_COUNT = 3
 FILENAME = 'problems_log.csv'
-LOG_MIN_NUMBER_LINES = 10000
-REDIS_HOST=''
-REDIS_PORT=0
+LOG_MIN_NUMBER_LINES = 15000
+REDIS_HOST= ''
+REDIS_PORT= 0
