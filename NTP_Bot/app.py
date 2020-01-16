f498811dd0c1643ab07d70117345782009bd4c83
JoaoVieira97 <jonasv55@hotmail.com>
2020-01-16 00:57:29 +0000

    Added /reset command to stop this mode at any time. Sending information to chat processor to send support lines to the user in certain cases. Other changes/fixes.

--- NTP_Bot/app.py
+++ NTP_Bot/app.py
@@ -17,46 +17,60 @@ def solve():
     msg = data['msg']
     
     ret_dict = {'chat_id': chat_id, 'msg': ''}
-    chat_id = str(chat_id) + '_redis'
+    chat_id = str(chat_id) + '_ntp'
     csr = redis_db.get(chat_id)
 
     save_on_redis = True
 
     if csr:
-        cs = json.loads(csr.decode('utf-8'))
-        exec_state = pickle.loads(base64.decodebytes(cs['state'].encode()))
-        
-        if exec_state.state == 0:
-            auth_data = msg.split(' ')
-            if len(auth_data) >= 2:
-                login = exec_state.setupSession(auth_data[0], auth_data[1])
-                if login == 0:
-                    ret_dict['msg'] = settings.UPROMPT[1]
-                    exec_state.state = exec_state.state + 1
-                    exec_state.error_count = 0
-                elif login == 1:
-                    exec_state.error_count = exec_state.error_count + 1
-                    if exec_state.error_count == settings.MAX_ERROR_COUNT:
-                        ret_dict['msg'] = settings.UPROMPT[7]
-                        ret_dict['chat_id'] = -1
-                        save_on_redis = False
+
+        if msg == '/reset':
+            save_on_redis = False
+            ret_dict['msg'] = settings.UPROMPT[12]
+
+        else:
+            cs = json.loads(csr.decode('utf-8'))
+            exec_state = pickle.loads(base64.decodebytes(cs['state'].encode()))
+
+            if exec_state.state == 0:
+                auth_data = msg.split(' ')
+                if len(auth_data) >= 2:
+                    login = exec_state.setupSession(auth_data[0], auth_data[1])
+                    if login == 0:
+                        ret_dict['msg'] = settings.UPROMPT[1]
+                        exec_state.state = exec_state.state + 1
+                        exec_state.error_count = 0
+                    elif login == 1:
+                        exec_state.error_count = exec_state.error_count + 1
+                        if exec_state.error_count == settings.MAX_ERROR_COUNT:
+                            ret_dict['msg'] = settings.UPROMPT[7]
+                            ret_dict['chat_id'] = -1
+                            save_on_redis = False
+                        else:
+                            ret_dict['msg'] = settings.UPROMPT[4]
                     else:
-                        ret_dict['msg'] = settings.UPROMPT[4]
+                        ret_dict['msg'] = settings.UPROMPT[5]
                 else:
                     ret_dict['msg'] = settings.UPROMPT[5]
-            else:
-                ret_dict['msg'] = settings.UPROMPT[5]
-                
-        elif exec_state.state == 1:
-            services_array = [(r't(ele)?v(isao)?', 'TV'), (r'televisão', 'TV'), (r'(inter)?net', 'Internet'), (r'wifi', 'Internet'), (r'voz', 'Voz')]
-            match_service = [x[1] for x in services_array if re.search(x[0], msg.lower())]
-            if match_service:
-                checker = exec_state.check_client_services(match_service[0])
-                if checker:
-                    exec_state.service = match_service[0]
-                    exec_state.state = exec_state.state + 1
-                    exec_state.error_count = 0
-                    ret_dict['msg'] = settings.UPROMPT[2]
+                    
+            elif exec_state.state == 1:
+                services_array = [(r't(ele)?v(isao)?', 'TV'), (r'televisão', 'TV'), (r'(inter)?net', 'Internet'), (r'wifi', 'Internet'), (r'voz', 'Voz')]
+                match_service = [x[1] for x in services_array if re.search(x[0], msg.lower())]
+                if match_service:
+                    checker = exec_state.check_client_services(match_service[0])
+                    if checker:
+                        exec_state.service = match_service[0]
+                        exec_state.state = exec_state.state + 1
+                        exec_state.error_count = 0
+                        ret_dict['msg'] = settings.UPROMPT[2]
+                    else:
+                        exec_state.error_count = exec_state.error_count + 1
+                        if exec_state.error_count == settings.MAX_ERROR_COUNT:
+                            ret_dict['msg'] = settings.UPROMPT[7]
+                            ret_dict['chat_id'] = -1
+                            save_on_redis = False
+                        else:
+                            ret_dict['msg'] = settings.UPROMPT[11]
                 else:
                     exec_state.error_count = exec_state.error_count + 1
                     if exec_state.error_count == settings.MAX_ERROR_COUNT:
@@ -64,67 +78,63 @@ def solve():
                         ret_dict['chat_id'] = -1
                         save_on_redis = False
                     else:
-                        ret_dict['msg'] = settings.UPROMPT[11]
-            else:
-                exec_state.error_count = exec_state.error_count + 1
-                if exec_state.error_count == settings.MAX_ERROR_COUNT:
-                    ret_dict['msg'] = settings.UPROMPT[7]
-                    ret_dict['chat_id'] = -1
-                    save_on_redis = False
+                        ret_dict['msg'] = settings.UPROMPT[3]
+                
+            elif exec_state.state == 2:
+                if exec_state.iter_deepening_search(msg):
+                    success, solution = exec_state.get_problem_solution()
+                    if success:
+                        if solution == settings.LINHAS_APOIO_SENTENCE:
+                            ret_dict['assunto'] = exec_state.service.lower()
+                        ret_dict['msg'] = 'Sugestão: ' + solution + '.\n\nResolveu o seu problema? (sim/não)'
+                        exec_state.state = exec_state.state + 1
+                        exec_state.error_count = 0
+                    else:
+                        ret_dict['msg'] = solution
                 else:
-                    ret_dict['msg'] = settings.UPROMPT[3]
-            
-        elif exec_state.state == 2:
-            if exec_state.iter_deepening_search(msg):
-                success, solution = exec_state.get_problem_solution()
-                if success:
-                    ret_dict['msg'] = 'Sugestão: ' + solution + '.\n\nResolveu o seu problema? (sim/não)'
-                    exec_state.state = exec_state.state + 1
-                    exec_state.error_count = 0
-                else:
-                    ret_dict['msg'] = solution
-            else:
-                exec_state.error_count = exec_state.error_count + 1
-                if exec_state.error_count == settings.MAX_ERROR_COUNT:
-                    ret_dict['msg'] = settings.UPROMPT[7]
-                    ret_dict['chat_id'] = -2
-                    save_on_redis = False
-                else:
-                    ret_dict['msg'] = settings.UPROMPT[6]
-
-        elif exec_state.state == 3:
-            options_array = [(r'\s*s(im)?', 1), (r'\s*n(ao)?', 0), (r'\s*não', 0)]
-            match_option = [x[1] for x in options_array if re.match(x[0], msg.lower())]
-            if match_option:
-                option = match_option[0]
-                if option: # problem has been resolved
-                    ret_dict['msg'] = settings.UPROMPT[9]
-                    ret_dict['chat_id'] = -1
-                    save_on_redis = False
-                    
-                    # write to log file of problems
-                    save_to_log(exec_state)
-                else: # problem has not been resolved
-                    if len(exec_state.suggestions) > exec_state.suggestion_count + 1: # there are still more suggestions to make
-                        exec_state.suggestion_count = exec_state.suggestion_count + 1
-                        new_suggestion = exec_state.suggestions[exec_state.suggestion_count]['prediction']
-                        ret_dict['msg'] = 'Outra sugestão: ' + new_suggestion + '.\n\nResolveu o seu problema? (sim/não)'
-                    else: # no more suggestions
-                        ret_dict['msg'] = settings.UPROMPT[10]
+                    exec_state.error_count = exec_state.error_count + 1
+                    if exec_state.error_count == settings.MAX_ERROR_COUNT:
+                        ret_dict['msg'] = settings.UPROMPT[7]
                         ret_dict['chat_id'] = -2
                         save_on_redis = False
-            else:
-                exec_state.error_count = exec_state.error_count + 1
-                if exec_state.error_count == settings.MAX_ERROR_COUNT:
-                    ret_dict['msg'] = settings.UPROMPT[7]
-                    ret_dict['chat_id'] = -1
-                    save_on_redis = False
-                else:
-                    ret_dict['msg'] = settings.UPROMPT[8]
+                    else:
+                        ret_dict['msg'] = settings.UPROMPT[6]
 
-        if save_on_redis:
-            cs['content'].append(msg)        
-            cs['state'] = base64.encodebytes(pickle.dumps(exec_state)).decode()
+            elif exec_state.state == 3:
+                options_array = [(r'\s*s(im)?', 1), (r'\s*n(ao)?', 0), (r'\s*não', 0)]
+                match_option = [x[1] for x in options_array if re.match(x[0], msg.lower())]
+                if match_option:
+                    option = match_option[0]
+                    if option: # problem has been resolved
+                        ret_dict['msg'] = settings.UPROMPT[9]
+                        ret_dict['chat_id'] = -1
+                        save_on_redis = False
+                        
+                        # write to log file of problems
+                        save_to_log(exec_state)
+                    else: # problem has not been resolved
+                        if len(exec_state.suggestions) > exec_state.suggestion_count + 1: # there are still more suggestions to make
+                            exec_state.suggestion_count = exec_state.suggestion_count + 1
+                            new_suggestion = exec_state.suggestions[exec_state.suggestion_count]['prediction']
+                            if new_suggestion == settings.LINHAS_APOIO_SENTENCE:
+                                ret_dict['assunto'] = exec_state.service.lower()
+                            ret_dict['msg'] = 'Outra sugestão: ' + new_suggestion + '.\n\nResolveu o seu problema? (sim/não)'
+                        else: # no more suggestions
+                            ret_dict['msg'] = settings.UPROMPT[10]
+                            ret_dict['chat_id'] = -2
+                            save_on_redis = False
+                else:
+                    exec_state.error_count = exec_state.error_count + 1
+                    if exec_state.error_count == settings.MAX_ERROR_COUNT:
+                        ret_dict['msg'] = settings.UPROMPT[7]
+                        ret_dict['chat_id'] = -1
+                        save_on_redis = False
+                    else:
+                        ret_dict['msg'] = settings.UPROMPT[8]
+
+            if save_on_redis:
+                cs['content'].append(msg)        
+                cs['state'] = base64.encodebytes(pickle.dumps(exec_state)).decode()
     else:
         ret_dict['msg'] = settings.UPROMPT[0]
         exec_state = ids.Inter_State()
@@ -140,8 +150,6 @@ def solve():
         response=json.dumps(ret_dict),
         mimetype='application/json'
     )
-
-
 
 
 def save_to_log(exec_state):
@@ -220,6 +228,7 @@ if __name__ == '__main__':
     # load model for sentences similarity
     msg_interpreter.loadModelData()
 
+    # start background work to retrain the model if necessary
     scheduler = BackgroundScheduler()
     scheduler.add_job(upload_csv, CronTrigger(hour=3)) # every day at 3AM
     scheduler.start()
--- NTP_Bot/settings.py
+++ NTP_Bot/settings.py
@@ -1,5 +1,5 @@
 UPROMPT = [
-    'É necessário realizar uma autenticação que mostre que és cliente da NOS.\nIndica o teu nº de telemóvel e o NIF separados por um espaço (exemplo \'930000000 123456789\').',
+    'Entraste no modo de resolução de problemas técnicos. Se em algum momento pretenderes sair deste modo utiliza o comando /reset.\n\nÉ necessário realizar uma autenticação que mostre que és cliente da NOS.\nIndica o teu nº de telemóvel e o NIF separados por um espaço (exemplo \'930000000 123456789\').',
     'Indica em qual dos seguintes serviços necessitas de assistência: TV, Internet, Voz.',
     'Descreve o problema em que necessitas de ajuda.',
     'Não foi possível reconhecer qual o serviço a que te referes, tenta novamente (TV, Internet ou Voz).',
@@ -8,9 +8,10 @@ UPROMPT = [
     'Não foi possível reconhecer o problema descrito, tenta descrever o mesmo novamente.',
     'Foi atingido o máximo de tentativas para esta etapa do processo. A sair do modo de resolução de problemas técnicos...',
     'Não foi possível reconhecer a tua mensagem. A sugestão resolveu o seu problema? (sim/não)',
-    'Ainda bem que o seu problema ficou resolvido. A sair do modo de resolução de problemas técnicos...',
-    'Não existem mais sugestões para resolver o seu problema, recomendamos que entre em contacto com as linhas de apoio. A sair do modo de resolução de problemas técnicos...',
-    'Não tens esse tipo de serviço contratado. Por favor, tenta novamente (TV, Internet ou Voz).'
+    'Ainda bem que o teu problema ficou resolvido. A sair do modo de resolução de problemas técnicos...',
+    'Não existem mais sugestões para resolver o teu problema, recomendamos que entres em contacto com as linhas de apoio. A sair do modo de resolução de problemas técnicos...',
+    'Não tens esse tipo de serviço contratado. Por favor, tenta novamente (TV, Internet ou Voz).',
+    'Cancelaste o processo de resolução de problemas técnicos. A sair deste modo...'
 ]
 SOLVER_ENDPOINT = 'http://{}:{}/problems/'
 SOLVER_ENDPOINT_LOGIN = SOLVER_ENDPOINT + 'login'
@@ -20,5 +21,6 @@ SOLVER_ENDPOINT_UPDATE_LOG = SOLVER_ENDPOINT + 'update_log'
 MAX_ERROR_COUNT = 3
 FILENAME = 'problems_log.csv'
 LOG_MIN_NUMBER_LINES = 15000
-REDIS_HOST= ''
-REDIS_PORT= 0
+REDIS_HOST = ''
+REDIS_PORT = 0
+LINHAS_APOIO_SENTENCE = 'linhas apoio'
